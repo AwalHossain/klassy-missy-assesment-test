d2074976dccfa709d8f93e802b96cc1febe2f21e
Awalhossain <waqin21@gmail.com>
2024-07-08 14:05:34 +0600

    feat: Add formReducer to the store for handling form state

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import formReducer from './formSlice'
 
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        form: formReducer,
+    },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
