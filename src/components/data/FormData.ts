72b165789538054b5e8f7e0dbbc9d2a0d753c900
Awalhossain <waqin21@gmail.com>
2024-07-09 17:33:00 +0600

    refactor: Update FormData.ts to add options for gender and concerns

---
+++ src/components/data/FormData.ts
@@ -0,0 +1,174 @@
+
+
+
+export const genderOptions = [
+    {
+        label: "Male",
+        value:"male"
+    },
+    {
+        label:"Female",
+        value:"Female"
+    }
+]
+
+// Skin Care
+
+// Hair Care
+
+// Beauty Care
+
+
+export const concernOptions = [
+ {
+        label: "Skin Care",
+        value:"Skin Care"
+    },
+    {
+        label:"Hair Care",
+        value:"Hair Care"
+    },
+    {
+        label:"Beauty Care",
+        value:"Beauty Care"
+ }
+]
+
+export const STEPS = [
+    {
+        id: 'Step 1',
+        name: 'Personal Information',
+        fields: ['name', 'gender', 'concern', 'DOB']
+    },
+    {
+        id: 'Step 2',
+        name: 'Concerns',
+        fields: ['concernName', 'eyeConcern', 'writtenConcern']
+    },
+    { id: 'Step 3', name: 'Complete' }
+]
+
+/**
+ * Acne & breakouts
+
+Blackheads
+
+Dry skin
+Fine lines & wrinkles
+
+Dehydrated skin
+
+Dull Skin
+
+Redness
+
+Excess oil
+
+Uneven skin texture
+
+Acne scars
+
+Acne Spot
+
+Hyperpigmentation
+ */
+
+
+export const concernNameOptions = [
+    {
+        label: "Acne & breakouts",
+        value:"Acne & breakouts"
+    },
+    {
+        label:"Blackheads",
+        value:"Blackheads"
+    },
+    {
+        label:"Dry skin",
+        value:"Dry skin"
+    },
+    {
+        label:"Fine lines & wrinkles",
+        value:"Fine lines & wrinkles"
+    },
+    {
+        label:"Dehydrated skin",
+        value:"Dehydrated skin"
+    },
+    {
+        label:"Dull Skin",
+        value:"Dull Skin"
+    },
+    {
+        label:"Redness",
+        value:"Redness"
+    },
+    {
+        label:"Excess oil",
+        value:"Excess oil"
+    },
+    {
+        label:"Uneven skin texture",
+        value:"Uneven skin texture"
+    },
+    {
+        label:"Acne scars",
+        value:"Acne scars"
+    },
+    {
+        label:"Acne Spot",
+        value:"Acne Spot"
+    },
+    {
+        label:"Hyperpigmentation",
+        value:"Hyperpigmentation"
+    }
+]
+
+
+export const eyeConcernOptions = [
+    {
+        label: "Dark circles",
+        value:"Dark circles"
+    },
+    {
+        label:"Puffiness",
+        value:"Puffiness"
+    },
+    {
+        label:"Fine lines & wrinkles",
+        value:"Fine lines & wrinkles"
+    },
+    {
+        label:"Dryness",
+        value:"Dryness"
+    },
+    {
+        label:"Dullness",
+        value:"Dullness"
+    },
+    {
+        label:"Uneven skin texture",
+        value:"Uneven skin texture"
+    },
+    {
+        label:"Redness",
+        value:"Redness"
+    },
+    {
+        label:"Sensitivity",
+        value:"Sensitivity"
+    },
+    {
+        label:"Acne",
+        value:"Acne"
+    },
+    {
+        label:"Oiliness",
+        value:"Oiliness"
+    },
+    {
+        label:"Pores",
+        value:"Pores"
+    },
+]
