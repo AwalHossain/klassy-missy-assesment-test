801773f665421a96856f89708ec1d2b691b4b0fb
Awalhossain <waqin21@gmail.com>
2024-07-09 15:51:31 +0600

    feat: Add lazy loading for images in FormStepThree component

---
+++ src/components/steps/FormStepThree.tsx
@@ -0,0 +1,44 @@
+import { Player } from '@lottiefiles/react-lottie-player';
+import { useEffect, useState } from 'react';
+import lottie from "../../../public/images/speed.json";
+import KanbanBoard from '../kanban/KanbanBoard';
+
+export default function FormStepThree() {
+    const [showContent, setShowContent] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowContent(true);
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+
+
+    return (
+        <div className="">
+            {!showContent ? (
+                <div className='w-full mx-auto'>
+                    <Player
+                        autoplay
+                        loop
+                        src={lottie}
+                        style={{ height: '150px', width: '300px' }}
+                        speed={2.5}
+                    >
+                    </Player>
+                    <div className="mb-6">
+                        <h1 className="text-[20px] font-semibold text-center">Analyzing Your Concern</h1>
+                        {/* <h2 className="text-[15px] font-semibold text-center">Thank you for sharing your concerns!</h2> */}
+                        <p className="text-center text-[10px]">
+                            We are analyzing your concerns and will get back to you with the best solution.
+                        </p>
+                    </div>
+                </div>
+            ) : (
+                <KanbanBoard />
+            )}
+        </div>
+    );
+}
