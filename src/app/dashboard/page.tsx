1ad03b193b80c3a5f5bb01dacba005f17a40bfee
Awalhossain <waqin21@gmail.com>
2024-07-09 10:44:20 +0600

    feat: Add DashboardPage component with KanbanBoard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import KanbanBoard from "@/components/CustomerUI/KanbanBoard";
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <KanbanBoard />
+        </div>
+    )
+}
