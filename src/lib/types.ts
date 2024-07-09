a4142b8bd6ef135a38de52d00c0be5a8561fcb51
Awalhossain <waqin21@gmail.com>
2024-07-09 07:06:07 +0600

    refactor: Add Card and Column interfaces to types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,3 +3,18 @@
 export interface ChildrenProps {
     children: React.ReactNode;
 }
+
+
+
+export interface Card {
+    id: string;
+    regimen: string;
+    timestamp: string;
+    name: string;
+  }
+  
+  export interface Column {
+    id: string;
+    title: string;
+    cards: Card[];
+  }
