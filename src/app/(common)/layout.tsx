e374d5b6a177eb0302a4dcfb53d570b399eab04a
Awalhossain <waqin21@gmail.com>
2024-07-09 07:05:58 +0600

    refactor: Update CustomerLayout component to improve layout and styling

--- src/app/(common)/layout.tsx
+++ src/app/(common)/layout.tsx
@@ -2,14 +2,14 @@ import { ChildrenProps } from "@/lib/types"
 
 const CustomerLayout = ({ children }: ChildrenProps) => {
     return (
-        <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg">
-            <h2 className="font-medium text-[18px] pt-5 pl-[13px]
-            ">Build Your Regimen
-            </h2>
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl bg-white rounded-lg shadow-lg p-4 md:p-8">
+                <h1 className="text-2xl md:text-3xl font-bold text-center mb-6">Build Your Regimen</h1>
 
-            <div className="w-full h-[2px] bg-[#EEE] my-2 px-2"></div>
-            <div className="pl-[13px]">
-                {children}
+                <div className="w-full h-[2px] bg-[#EEE] my-2"></div>
+                <div className="">
+                    {children}
+                </div>
             </div>
         </div>
     )
