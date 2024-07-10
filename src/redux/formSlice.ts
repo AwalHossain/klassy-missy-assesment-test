444121d8b4d42633b77784056e99ca2317185344
Awalhossain <waqin21@gmail.com>
2024-07-10 10:32:38 +0600

    refactor: Update formSlice to include currentStep property and implement local storage saving and loading

--- src/redux/formSlice.ts
+++ src/redux/formSlice.ts
@@ -1,44 +1,60 @@
 import { FormDataSchema } from '@/lib/formSchema';
+import { getFromLocalStorage, setToLocalStorage } from '@/utils/localstorage';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { z } from 'zod';
-export type FormStateslice = z.infer<typeof FormDataSchema>;
+
+export type FormStateslice = z.infer<typeof FormDataSchema> & { currentStep: number };
+
+const loadState = (): FormStateslice | undefined => {
+  try {
+    const serializedState = getFromLocalStorage('skinRegimenForm');
+    return serializedState ? JSON.parse(serializedState) : undefined;
+  } catch (err) {
+    console.error("Could not load state from localStorage", err);
+    return undefined;
+  }
+};
+
+const saveState = (state: FormStateslice) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    setToLocalStorage('skinRegimenForm', serializedState);
+  } catch (err) {
+    console.error("Could not save state to localStorage", err);
+  }
+};
 
 const initialState: FormStateslice = {
   name: '',
   gender: '',
   concern: '',
   DOB: new Date(),
-  concernName: [], // Initialize as an empty array
+  concernName: [],
   eyeConcern: '',
   writtenConcern: '',
+  currentStep: 0,
 };
 
 const formSlice = createSlice({
   name: 'form',
-  initialState,
+  initialState: loadState() || initialState,
   reducers: {
-    updateField: (state, action: PayloadAction<{ field: keyof FormStateslice; value: any }>) => {
-      const { field, value } = action.payload;
-      (state[field] as any) = value;
-    },
     setFormData: (state, action: PayloadAction<Partial<FormStateslice>>) => {
-      return { ...state, ...action.payload };
+      const newState = { ...state, ...action.payload };
+      saveState(newState);
+      return newState;
     },
-    resetForm: () => initialState,
+    setCurrentStep: (state, action: PayloadAction<number>) => {
+      state.currentStep = action.payload;
+      saveState(state);
+    },
+    resetForm: () => {
+      localStorage.removeItem('skinRegimenForm');
+      return initialState;
+    },
   },
 });
 
-
-export const { updateField, setFormData, resetForm } = formSlice.actions;
+export const { setFormData, setCurrentStep, resetForm } = formSlice.actions;
 
 export default formSlice.reducer;
-// a function to save the form state to localStorage
-export const saveFormState = (state: FormStateslice) => {
-  localStorage.setItem('formData', JSON.stringify(state));
-};
-
-//  a function to load the form state from localStorage
-export const loadFormState = (): FormStateslice | null => {
-  const state = localStorage.getItem('formData');
-  return state ? JSON.parse(state) as FormStateslice : null;
-};
