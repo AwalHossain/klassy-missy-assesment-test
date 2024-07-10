1ce7a0341fc748062a09d08b1e0b82c04175ff39
Awalhossain <waqin21@gmail.com>
2024-07-10 10:32:26 +0600

    refactor: Add gender, concern, DOB, concernName, eyeConcern, and writtenConcern properties to KanbanCard interface

--- src/redux/types/kanbanTypes.ts
+++ src/redux/types/kanbanTypes.ts
@@ -1,9 +1,15 @@
 export interface KanbanCard {
-    id: string;
-    regimen: string;
-    datetime: string;
-    name: string;
-  }
+  id: string;
+  regimen: string;
+  datetime: string;
+  name: string;
+  gender?: string;
+  concern?: string;
+  DOB?: Date; // Keeping DOB as Date since you wanted all except date to be string
+  concernName?: string[];
+  eyeConcern?: string;
+  writtenConcern?: string;
+}
   
   export interface KanbanColumn {
     title: string;
