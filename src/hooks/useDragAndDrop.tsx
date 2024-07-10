355a4e27e2fe5a381a66034aea30a3f1e24a5670
Awalhossain <waqin21@gmail.com>
2024-07-10 10:59:03 +0600

    refactor: Refactor useDragAndDrop to dispatch moveCard action instead of updating state directly

--- src/hooks/useDragAndDrop.tsx
+++ src/hooks/useDragAndDrop.tsx
@@ -1,50 +1,33 @@
+import { useAppDispatch } from '@/redux/hooks';
+import { moveCard } from '@/redux/kanbanSlice';
 import { DropResult } from 'react-beautiful-dnd';
-import { Column } from '../lib/types';
 
-const useDragAndDrop = (initialColumns: Column[], setColumns: React.Dispatch<React.SetStateAction<Column[]>>) => {
+const useDragAndDrop = () => {
+    const dispatch = useAppDispatch();
+
     const onDragEnd = (result: DropResult) => {
         const { source, destination } = result;
 
-        if (!destination) return;
-
-        if (
-            source.droppableId === destination.droppableId &&
-            source.index === destination.index
-        ) {
+        if (!destination) {
             return;
         }
 
-        const sourceColumn = initialColumns.find(col => col.id === source.droppableId);
-        const destColumn = initialColumns.find(col => col.id === destination.droppableId);
-
-        if (!sourceColumn || !destColumn) return;
-
-        const sourceCards = Array.from(sourceColumn.cards);
-        const [removedCard] = sourceCards.splice(source.index, 1);
-
-        if (source.droppableId === destination.droppableId) {
-            sourceCards.splice(destination.index, 0, removedCard);
-            const newColumns = initialColumns.map(col =>
-                col.id === sourceColumn.id ? { ...col, cards: sourceCards } : col
-            );
-            setColumns(newColumns);
-        } else {
-            const destCards = Array.from(destColumn.cards);
-            destCards.splice(destination.index, 0, removedCard);
-            const newColumns = initialColumns.map(col => {
-                if (col.id === sourceColumn.id) {
-                    return { ...col, cards: sourceCards };
-                }
-                if (col.id === destColumn.id) {
-                    return { ...col, cards: destCards };
-                }
-                return col;
-            });
-            setColumns(newColumns);
+        if (
+            source.droppableId !== destination.droppableId ||
+            source.index !== destination.index
+        ) {
+            dispatch(moveCard({
+                sourceColumnId: source.droppableId,
+                destinationColumnId: destination.droppableId,
+                sourceIndex: source.index,
+                destinationIndex: destination.index,
+            }));
         }
     };
 
-    return { onDragEnd };
+    return {
+        onDragEnd,
+    };
 };
 
 export default useDragAndDrop;
