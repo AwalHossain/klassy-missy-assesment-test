75a1ffe575001fec088eae1c28e943823f8a5ac6
Awalhossain <waqin21@gmail.com>
2024-07-10 10:58:50 +0600

    refactor: Update KanbanBoard to use custom hook for drag and drop functionality

--- src/components/kanban/KanbanBoard.tsx
+++ src/components/kanban/KanbanBoard.tsx
@@ -1,39 +1,20 @@
 import React from 'react';
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext } from 'react-beautiful-dnd';
 
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { moveCard } from '@/redux/kanbanSlice';
+import useDragAndDrop from '@/hooks/useDragAndDrop';
+import { useAppSelector } from '@/redux/hooks';
 import Column from './Column';
 
 const KanbanBoard: React.FC = () => {
-    const dispatch = useAppDispatch();
+
+    const { onDragEnd } = useDragAndDrop();
     const { columns } = useAppSelector((state) => state.kanban);
 
     console.log(columns, 'columns from kanban board');
 
 
-    const onDragEnd = (result: DropResult) => {
-        const { source, destination } = result;
-
-        if (!destination) {
-            return;
-        }
-
-        if (
-            source.droppableId !== destination.droppableId ||
-            source.index !== destination.index
-        ) {
-            dispatch(moveCard({
-                sourceColumnId: source.droppableId,
-                destinationColumnId: destination.droppableId,
-                sourceIndex: source.index,
-                destinationIndex: destination.index,
-            }));
-        }
-    };
-
     return (
-        <div className="w-full h-screen p-4 overflow-x-auto md:overflow-x-visible">
+        <div className="w-full p-4 overflow-x-auto md:overflow-x-visible">
             <div className="inline-flex md:flex md:flex-wrap gap-4 min-w-full">
                 <DragDropContext onDragEnd={onDragEnd}>
                     {Object.entries(columns).map(([columnId, column]) => (
