8b1c9e68af4bbf89c0067b3b4415f1943fe7f584
Awalhossain <waqin21@gmail.com>
2024-07-10 10:31:50 +0600

    refactor: Update CharacterImage component with larger image size and improved styling

--- src/components/ui/characterImage.tsx
+++ src/components/ui/characterImage.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 
 const CharacterImage = ({ ImageUrl }: { ImageUrl: string }) => (
     <div className="flex justify-center mb-[15px]">
-        <div className="relative">
-            <Image src={ImageUrl} width="111" height={111} alt="Character" className="rounded-full" />
+        <div className="relative w-">
+            <Image src={ImageUrl} width="150" height={150} alt="Character" className="rounded-full" />
         </div>
     </div>
 );
