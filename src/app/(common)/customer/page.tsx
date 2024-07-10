c7d41ef6f15a643d52ad56ec20682d29955c175a
Awalhossain <waqin21@gmail.com>
2024-07-10 10:30:15 +0600

    refactor: Add lazy loading for SkinRegimenForm component

--- src/app/(common)/customer/page.tsx
+++ src/app/(common)/customer/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import dynamic from 'next/dynamic';
+import { Suspense } from "react";
 
 const SkinRegimenForm = dynamic(() => import('@/components/CustomerUI/SkinRegimenForm'), { ssr: false })
 
@@ -11,7 +12,9 @@ export default function CustomerPage() {
                 <Button className="w-[131px] h-[25px] bg-black py-1 px-[13px] rounded-[3px]">Login to Klassy</Button>
                 <p className="text-[#f90] text-[10px] pt-[9px]">* Login to sync your profile details for more easy step.</p>
             </div>
-            <SkinRegimenForm />
+            <Suspense fallback={<div>Loading...</div>}>
+                <SkinRegimenForm />
+            </Suspense>
         </div>
     )
 }
