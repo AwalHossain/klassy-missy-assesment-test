ea666ef65ac60690940cf93e2e03883d77f3bb02
Awalhossain <waqin21@gmail.com>
2024-09-03 20:28:32 +0600

    refactor: Add password and confirmPassword fields to FormikForm component for form submission handling

--- src/app/formik/page.tsx
+++ src/app/formik/page.tsx
@@ -1,39 +1,84 @@
 'use client';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import formikSchema from "@/lib/formikSchemas";
 import { useFormik } from "formik";
 
 // form value interface
 interface FormValues {
     username: string;
     email: string;
+    password: string;
+    confirmPassword: string;
 }
 
 export default function FormikForm() {
-    const handFormleSubmit = (values: FormValues) => {
-        console.log(values)
-    }
-    const { values, handleChange, handleBlur, handleSubmit } = useFormik({
+    const handleSubmit = (values: FormValues) => {
+        console.log(values);
+    };
+
+    const { values, errors, handleChange, handleBlur, handleSubmit: formikSubmit, touched } = useFormik({
         initialValues: {
             username: '',
-            email: ''
+            email: '',
+            password: '',
+            confirmPassword: ''
         },
-        onSubmit: handFormleSubmit,
-    })
+        onSubmit: handleSubmit,
+        validationSchema: formikSchema
+    });
 
-    console.log('formik vales', values);
-
+    console.log('formik values', values, 'formik errors', errors);
 
     return (
-        // <Formik>
-
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Write your username</label>
-            <Input type="text" name="username" value={values.username} onChange={handleChange} />
-            <label htmlFor="email">Write your email</label>
-            <Input type="email" name="email" value={values.email} onChange={handleChange} />
-            <Button type="submit">Submit</Button>
-        </form>
-        // </Formik>
-    )
+        <div className="max-w-[550px] mx-auto">
+            <form onSubmit={formikSubmit}>
+                <div className="mb-4">
+                    <label htmlFor="username">Write your username</label>
+                    <Input
+                        type="text"
+                        name="username"
+                        value={values.username}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                    {errors.username && touched.username && <span className="text-red-500">{errors.username}</span>}
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="email">Write your email</label>
+                    <Input
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                    {errors.email && touched.email && <span className="text-red-500">{errors.email}</span>}
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="password">Password</label>
+                    <Input
+                        type="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                    {errors.password && touched.password && <span className="text-red-500">{errors.password}</span>}
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <Input
+                        type="password"
+                        name="confirmPassword"
+                        value={values.confirmPassword}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                    {errors.confirmPassword && touched.confirmPassword && <span className="text-red-500">{errors.confirmPassword}</span>}
+                </div>
+                <Button type="submit" className="my-2">Submit</Button>
+            </form>
+        </div>
+    );
 }
